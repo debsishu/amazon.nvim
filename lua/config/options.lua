e346fed3ca7716c78101334d2c19e3281de05301
debsishu <pauldebsishu2002@gmail.com>
2025-05-09 19:42:42 +0530

    bugfix: branch change for vimion

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,7 @@ opt.clipboard = ""
 vim.g.autoformat = false
 vim.o.guifont = "GeistMono Nerd Font:h17"
 vim.opt.linespace = 5
+vim.opt.termguicolors = true
+-- for dev desk setup
+vim.o.lazyredraw = true
+vim.o.ttyfast = true
--- lua/plugins/vimion.lua
+++ lua/plugins/vimion.lua
@@ -3,6 +3,6 @@ return {
     -- https://w.amazon.com/bin/view/VimIon
     lazy = true,
     url = "ssh://git.amazon.com:2222/pkg/VimIon.git",
-    branch = "master",
+    branch = "mainline",
   },
 }
