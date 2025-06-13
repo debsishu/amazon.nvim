b4274ddd5d52193e01bb570d501b23b3ed640576
Debsishu Pal <pauldebsishu2002@gmail.com>
2025-06-14 03:53:13 +0530

    chore: added IST time along with UTC time in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,15 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    local function utc_time()
-      return os.date("%H:%M UTC")
-    end
     opts.sections.lualine_z = {
-      utc_time,
+      function()
+        local utc = os.time(os.date("!*t"))
+        local ist = utc + 19800
+        return "IST " .. os.date("%H:%M", ist)
+      end,
+      function()
+        return "UTC " .. os.date("!%H:%M") -- '!' gets UTC time
+      end,
     }
     opts.options = {
       component_separators = { left = "|", right = "|" },
