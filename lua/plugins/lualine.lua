cf759e6ad6da742ad2c8360eb6df740a7e25fa4a
Debsishu Pal <pauldebsishu2002@gmail.com>
2025-06-14 03:48:37 +0530

    chore: added UTC time in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,16 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  opts = {
-    options = {
+  opts = function(_, opts)
+    local function utc_time()
+      return os.date("%H:%M UTC")
+    end
+    opts.sections.lualine_z = {
+      utc_time,
+    }
+    opts.options = {
       component_separators = { left = "|", right = "|" },
       section_separators = {},
-    },
-  },
+    }
+  end,
 }
